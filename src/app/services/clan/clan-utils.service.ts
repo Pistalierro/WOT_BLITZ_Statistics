b971a77b94759643186b77a8ffa5605cce85bd0b
Anatoliy Sysoienko <pistalierro@gmail.com>
2025-02-13 18:25:43 +0200

    Убрал резолвер и оптимизировал батчами изменил

--- src/app/services/clan/clan-utils.service.ts
+++ src/app/services/clan/clan-utils.service.ts
@@ -170,7 +170,6 @@ export class ClanUtilsService {
     }
   }
 
-
   chunkArray<T>(array: T[], size: number): T[][] {
     return Array.from({length: Math.ceil(array.length / size)}, (_, i) =>
       array.slice(i * size, i * size + size)
