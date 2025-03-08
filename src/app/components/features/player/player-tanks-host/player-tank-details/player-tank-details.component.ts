9ab2becb3f7ae50de180269c876b0dc952bd9543
Anatoliy Sysoienko <pistalierro@gmail.com>
2025-03-08 18:41:05 +0200

    переместил компонент

---
+++ src/app/components/features/player/player-tanks-host/player-tank-details/player-tank-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-tank-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './player-tank-details.component.html',
+  styleUrl: './player-tank-details.component.scss'
+})
+export class PlayerTankDetailsComponent {
+
+}
